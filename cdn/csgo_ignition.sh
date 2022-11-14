6f66ed074af30fb272c02bcc9ddeb14b2a31bcb3
SuperHori <xanlua69@gmail.com>
2022-11-14 23:16:11 +0200

    Deleted unused files

--- cdn/csgo_ignition.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-GSLT=25C13BCAA62CECA1864A0A91929B3622
-screen -dm bash -c "cd /root/csgo-server && bash /root/csgo-server/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount $GSLT"
